8317ee27fac4031cabc96beb6ef0db658db807cb
IlijaTrifke <ilijatrifke59@gmail.com>
2025-11-03 16:53:50 +0100

    chore: setup redhat formatter and set url for h2 DB to be testdb instead of vehicledb

--- src/main/java/com/meuhlbauer/vehicle_backend/VehicleBackendApplication.java
+++ src/main/java/com/meuhlbauer/vehicle_backend/VehicleBackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VehicleBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VehicleBackendApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(VehicleBackendApplication.class, args);
+  }
 }
